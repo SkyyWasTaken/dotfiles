a7a4c52e97a0d2868e10f0f30e319bfe0964501e
SkyyWasTaken <coolg0092002@gmail.com>
2026-01-26 21:52:58 -0500

    Removed unnecessary comments in configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -131,19 +131,6 @@ in
     proggyfonts
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [
     57621  # Spotify local track sync
